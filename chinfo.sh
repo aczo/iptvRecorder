13566c3bbc4bfcde904135360bcff764204f9b68
aczo <adam.t.czopek@gmail.com>
2024-08-13 08:03:51 +0200

    Removed IDE comments

--- chinfo.sh
+++ chinfo.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 . env.sh
-# shellcheck disable=SC2164
-# shellcheck disable=SC2164
 cd $PYTHON_DIR
 ffprobe -print_format json -show_programs udp://$($PYTHON getcurstream.py)":$UDP_PORT"
